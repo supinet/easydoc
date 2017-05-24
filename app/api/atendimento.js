95526831c838225040123aad7e26d73b31d58f85
supinet <fernando.supi@gmail.com>
2017-05-23 21:55:51 -0400

    added func findById

--- app/api/atendimento.js
+++ app/api/atendimento.js
@@ -1,11 +1,19 @@
 var api = {};
 
+var atendimentos = [
+    {_id: 1, tipo_atendimento: 'PRIMARIO', retorno: 'NÃO'},
+    {_id: 2, tipo_atendimento: 'PRIMARIO', retorno: 'SIM'},
+    {_id: 3, tipo_atendimento: 'PRIMARIO', retorno: 'NÃO'}
+];
+ 
 api.lista = function(req, res) {
-    var atendimentos = [
-                {_id: 1, tipo_atendimento: 'PRIMARIO', retorno: 'NÃO'},
-                {_id: 2, tipo_atendimento: 'PRIMARIO', retorno: 'SIM'},
-                {_id: 3, tipo_atendimento: 'PRIMARIO', retorno: 'NÃO'}
-            ];
-            res.json(atendimentos);
+    res.json(atendimentos);
+};
+
+api.buscaPorId = function(req, res) {
+    var atendimento = atendimentos.find(function(atendimento) {
+        return atendimento._id == req.params.id;
+    });
+    res.json(atendimento);
 }
 module.exports = api;
--- app/routes/atendimento.js
+++ app/routes/atendimento.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
     var api = app.api.atendimento;
     app.get('/v1/atendimentos', api.lista);
+    app.get('/v1/atendimentos/:id', api.buscaPorId);
 }
